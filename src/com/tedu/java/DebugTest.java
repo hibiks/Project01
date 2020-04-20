53ed7581ef1dc386d49f93c1f8c90d4089857bb4
hyacthus <alettachloe9@gmail.com>
2020-04-20 16:09:25 +0800

    debug测试

---
+++ src/com/tedu/java/DebugTest.java
@@ -0,0 +1,19 @@
+package com.tedu.java;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DebugTest {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("name","Tome");
+        map.put("age","19");
+        map.put("school","sqinghua");
+        map.put("name","computme");
+        System.out.println("map = " + map);
+
+
+    }
+
+
+}
